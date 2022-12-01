c127e50802190eb20b0863a074c702ff0fb4d1e6
sysialvarez <ismacorporation@gmail.com>
2022-12-01 15:56:30 +0100

    Subimos los archivos necesarios para el examen

---
+++ chat_using_sockets/.ipynb_checkpoints/cliente-checkpoint.py
@@ -0,0 +1,36 @@
+import threading
+import sys
+import socket
+import pickle
+import os
+
+class Cliente():
+
+	def __init__(self, host=input("Intoduzca la IP del servidor ?  "), port=int(input("Intoduzca el PUERTO del servidor ?  "))):
+		self.s = socket.socket()
+		self.s.connect((host, int(port)))
+		print('\n\tProceso con PID = ',os.getpid(), '\n\tHilo PRINCIPAL con ID =',threading.currentThread().getName(), '\n\tHilo en modo DAEMON = ', threading.currentThread().isDaemon(),'\n\tTotal Hilos activos en este punto del programa =', threading.active_count())
+		threading.Thread(target=self.recibir, daemon=True).start()
+
+		while True:
+			msg = input('\nEscriba texto ?   ** Enviar = ENTER   ** Salir Chat = 1 \n')
+			if msg != '1' : self.enviar(msg)
+			else:
+				print(" **** Me piro vampiro; cierro socket y mato al CLIENTE con PID = ", os.getpid())
+				self.s.close()
+				sys.exit()
+
+	def recibir(self):
+		print('\nHilo RECIBIR con ID =',threading.currentThread().getName(), '\n\tPertenece al PROCESO con PID', os.getpid(), "\n\tHilos activos TOTALES ", threading.active_count())
+		while True:
+			try:
+				data = self.s.recv(32)
+				if data: print(pickle.loads(data))
+			except: pass
+
+	def enviar(self, msg):
+		self.s.send(pickle.dumps(msg))
+
+arrancar = Cliente()
+
+		
